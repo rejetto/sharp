cb5c08e84304c64904afc0f64130a0aa5926c1b5
Massimo Melina <a@rejetto.com>
2023-09-27 11:24:41 +0200

    fix: "A string was expected" error

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,4 +1,4 @@
-exports.version = 1.0
+exports.version = 1.01
 exports.description = "Thumbnails engine. Install this and then the thumbnails plugin."
 exports.apiRequired = 8.21 // customApi
 exports.repo = "rejetto/sharp"
