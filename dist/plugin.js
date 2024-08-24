7e66c9494cafd5a76b7dd85c8fd1ba2b8e433004
Massimo Melina <a@rejetto.com>
2024-08-24 16:37:26 +0200

    added support for linux-arm64 musl

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,4 +1,4 @@
-exports.version = 1.02
+exports.version = 1.1
 exports.description = "Thumbnails engine. Install this and then the thumbnails plugin."
 exports.apiRequired = 8.21 // customApi
 exports.repo = "rejetto/sharp"
